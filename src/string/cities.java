eed6a4edd5a2558c7248c269844de3f3fe82f058
SSI STUDENT <SSI STUDENT@HP08-PC>
2019-08-29 17:08:10 +0530

    string

---
+++ src/string/cities.java
@@ -0,0 +1,33 @@
+package string;
+
+public class cities {
+	
+		public static void main(String[]args)
+		{
+		
+		int i=0,gc=0,yc=0,oc=0,j,k,count=0;
+		String g="gmail",y="yahoo",r="outlook";	
+		String mail[]={"Indore","Bhopal","Agra","Pune","Delhi","Chennai"};
+		for( i=0;i<mail.length;i++)
+			
+		{
+			for(j=i+1;j<mail.length;j++)
+			{
+				if(mail[i].[0])
+			char h=g[2];
+				
+				
+			}
+			
+			
+		}
+		System.out.println("gmail  "+gc);
+		System.out.println("yahoo  "+yc);
+		System.out.println("outlook  "+oc);
+		System.out.println("Total Domain   "+count);
+		}
+		
+	
+
+
+}
